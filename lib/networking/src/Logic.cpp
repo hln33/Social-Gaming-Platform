ba3a08c4663990ca10ec12e67418280c4b9e952b
Jiaming Gong <jga104@sfu.ca>
2022-10-07 18:54:16 -0700

    have the public class for logic

--- lib/networking/src/Logic.cpp
+++ lib/networking/src/Logic.cpp
@@ -1,1 +1,23 @@
+/////////////////////////////////////////////////////////////////////////////
+//                         Business Logic Layer
+//
+// This file contains the implementation of the business logic layer
+/////////////////////////////////////////////////////////////////////////////
+
+#include "Logic.h"
+
+Class Logic{
+    public:
+        Logic();
+        ~Logic();
+        void run();
+        void stop();
+        void send(std::string message);
+        void receive(std::string message);
+        void connect(std::string address, std::string port);
+        void disconnect();
+        void setCallback(std::function<void(std::string)> callback);
+
+    
+};
 
