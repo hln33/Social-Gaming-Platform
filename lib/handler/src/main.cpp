0574bbb63f21b784dfb6eb56e5588aca46ec0ccc
hln5 <hln5@sfu.ca>
2022-10-18 10:03:56 -0700

    removing placeholder test in lib/handler/main.cpp because googletest is now integrated

--- lib/handler/src/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "../include/handler.h"
-#include <string>
-
-// use this main function to test handler code for now. In the future we will probably use unit tests instead.
-int main() {
-    std::string test;
-    recieveMessage(test);
-}
