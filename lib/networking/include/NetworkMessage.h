68225ec6d19c0f9ccc99f4ca46094215dd3dbc96
hln5 <hln5@sfu.ca>
2022-10-24 19:00:19 -0700

    resolving compilation errors in terminalServer code

--- lib/networking/include/NetworkMessage.h
+++ lib/networking/include/NetworkMessage.h
@@ -1,14 +1,13 @@
-#include<string>
+#pragma once
 
+#include<string>
 
 struct messageType{
     std::string JOIN = "Join";
     std::string CREATE = "Create";
-    std::string Data = "Data";
-    std::string CREATE = "Create";
+    std::string DATA = "Data";
     std::string SUCCESS = "Success";
     std::string QUIT = "Quit";
     std::string CLOSE_GAME = "close game";
-
-
-};
+    std::string SHUTDOWN = "shutdown";
+}; 
