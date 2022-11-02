2275dd57b8c16b4a526e7e32ab2629099e422c69
matheww <matheww@asb9700u-g02.csil.sfu.ca>
2022-11-01 18:43:37 -0700

    resolving revert issue

---
+++ lib/gameLogic/includes/player_storage.h
@@ -0,0 +1,46 @@
+#include <unordered_map>
+
+#include "unique_id.h"
+
+// default hash
+
+class IPlayer {
+public:
+    virtual void setPublicId(IUniqueId);
+
+    virtual IUniqueId getPublicId() const;
+};
+
+namespace std {
+    template<>
+    struct hash<IPlayer> {
+        std::size_t operator()(IPlayer const& p) const noexcept {
+            return std::hash<std::string>{}(p.getPublicId().getValue());
+        }
+    };
+}
+
+
+
+
+class IPlayerStorage {
+public:
+    virtual void addPlayerRecord(IPlayer&);
+    virtual void updatePlayerRecord(IPlayer&);
+    virtual void removePlayerRecord(IUniqueId&);
+    virtual const IPlayer& getPlayerRecord(IUniqueId&) const;
+    virtual int getNumPlayerRecords() const;
+};
+
+class PlayerStorage : public IPlayerStorage {
+public:
+    virtual void addPlayerRecord(IPlayer&) override;
+    virtual void updatePlayerRecord(IPlayer&);
+    virtual void removePlayerRecord(IUniqueId&) override;
+    virtual const IPlayer& getPlayerRecord(IUniqueId&) const override;
+    virtual int getNumPlayerRecords() const override;
+
+private:
+    std::unordered_map<IUniqueId, IPlayer> players;
+};
+
---
+++ lib/gameLogic/src/player_storage.cpp
@@ -0,0 +1,24 @@
+#include <unordered_map>
+
+#include "../includes/unique_id.h"
+#include "../includes/player_storage.h"
+
+void PlayerStorage::addPlayerRecord(IPlayer &p) {
+    this->players.insert(std::make_pair(p.getPublicId(), p));
+}
+
+void PlayerStorage::updatePlayerRecord(IPlayer &p) {
+    this->players.insert_or_assign(p.getPublicId(), p);
+}
+
+void PlayerStorage::removePlayerRecord(IUniqueId& key) {
+    this->players.erase(key);
+}
+
+const IPlayer& PlayerStorage::getPlayerRecord(IUniqueId& key) const {
+    return this->players.at(key);
+}
+
+int PlayerStorage::getNumPlayerRecords() const {
+    return this->players.size();
+}
