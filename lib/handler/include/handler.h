ae5eeed71112a4fafa41092281d976039af2f30b
nhanvyn <nhanvyn@sfu.ca>
2022-11-06 10:41:15 -0800

    added helpers function to serialize json to config and constant struct

--- lib/handler/include/handler.h
+++ lib/handler/include/handler.h
@@ -13,19 +13,72 @@ enum class parse_event_t : std:: uint8_t{
     value
 };
 
+// struct Config {
+//     // for storing key->value for configuration, constant, etc...
+//     std::string name;
+//     int maxPlayer;
+//     int minPlayer;
+//     bool allowsAudience;
+
+//     std::unordered_map<std::string, std::string> setup;
+// };
 struct Config {
     // for storing key->value for configuration, constant, etc...
-    std::string name;
-    int maxPlayer;
-    int minPlayer;
-    bool allowsAudience;
-
     std::unordered_map<std::string, std::string> setup;
+    
+    int getMaxPlayer(){
+        if (setup.find("max") != setup.end()){
+            return std::stoi(setup["max"]);
+        }    
+        return -1;
+    }
+
+    int getMinPlayer(){
+        if (setup.find("min") != setup.end()){
+            return std::stoi(setup["min"]);
+        }    
+        return -1;
+    }
+
+    std::string getName(){
+        if (setup.find("name") != setup.end()){
+            return setup["name"];
+        }
+        return "";    
+    }
 };
 
 struct JsonLookup {
     std::unordered_map<std::string, std::string> valueLookUp;
 };
+
+struct Pool {
+    //Usage: weaponLookUp["name"] = {rock, paper, scissor}, weaponLookUp["beats"] = {scissors, rock, paper}
+    std::unordered_map<std::string, std::vector<std::string>> weaponLookUp;
+};
+
+struct Constant {
+    std::unordered_map<std::string, Pool> lists;
+
+    void printConstant(){
+        Pool pool = (*lists.begin()).second; // access the list inside weapon
+
+        std::cout << "=================================\n";
+        
+
+        for (auto const &pair: pool.weaponLookUp) {
+            std::cout <<"TYPE: " << pair.first << "\n";
+            std::cout <<"SIZE: " << pair.second.size() << "\n";
+            for (auto const& v : pair.second){
+                std::cout << " items = " << v;
+            }
+            std::cout << "\n";
+        }
+        std::cout << "=================================\n";
+
+    }
+};
+
 
 
 using json = nlohmann::json;
