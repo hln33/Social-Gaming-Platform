f4fc93b8787e4ff219549dfb1de69e69e2d8929f
hln5 <hln5@sfu.ca>
2022-11-16 19:26:45 -0800

    added additonal tests. I also noticed that we have a bug in controller. It has to do with incorrect player connections being returned after joining a room

--- test/controllerTests.cpp
+++ test/controllerTests.cpp
@@ -1,5 +1,6 @@
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
+#include <spdlog/spdlog.h>
 
 #include "controller.h"
 
@@ -7,7 +8,15 @@ using namespace testing;
 
 class ControllerTests : public Test {
     protected:
-        void SetUp() override { }
+        void SetUp() override {
+            player1.id = 111;
+            player2.id = 222;
+            player3.id = 333;
+            player4.id = 444;
+
+            auto response = controller.createRoom(config, player1);
+            inviteCode = response.data.message;
+        }
 
     Controller controller;
     json config = json::parse(R"(
@@ -22,17 +31,61 @@ class ControllerTests : public Test {
             }
         }
     )");
+
+    networking::Connection player1;
+    networking::Connection player2;
+    networking::Connection player3;
+    networking::Connection player4;
+
+    std::string inviteCode;
 };
 
 TEST_F(ControllerTests, createRoom) {
-    networking::Connection player;
-    player.id = 111;
-
-    recipientsWrapper response = controller.createRoom(config, player);
+    auto response = controller.createRoom(config, player1);
     auto recipients = response.recipientList;
 
     EXPECT_EQ(response.data.status, Status::SUCCESS);
     EXPECT_FALSE(response.data.message == ""); // game code should created
-    EXPECT_FALSE(recipients.find(player) == recipients.end());
+    EXPECT_FALSE(recipients.find(player1) == recipients.end());
     EXPECT_EQ(recipients.size(), 1);
+}
+
+TEST_F(ControllerTests, onePlayerJoinsRoom) {
+    auto response = controller.joinRoom(inviteCode, player2);
+    auto recipients = response.recipientList;
+
+    EXPECT_EQ(response.data.status, Status::SUCCESS);
+    EXPECT_EQ(recipients.size(), 2);
+
+    for_each(recipients.begin(), recipients.end(), [](const networking::Connection& recipient){
+        SPDLOG_INFO(recipient.id);
+    });
+
+    EXPECT_FALSE(recipients.find(player2) == recipients.end());
+}
+
+TEST_F(ControllerTests, multiplePlayersJoinRoom) {
+    auto response = controller.joinRoom(inviteCode, player2);
+    auto recipients = response.recipientList;
+
+    EXPECT_EQ(response.data.status, Status::SUCCESS);
+    EXPECT_EQ(recipients.size(), 2);
+    EXPECT_FALSE(recipients.find(player2) == recipients.end());
+
+    response = controller.joinRoom(inviteCode, player3);
+    recipients = response.recipientList;
+
+    EXPECT_EQ(response.data.status, Status::SUCCESS);
+    EXPECT_EQ(recipients.size(), 3);
+    EXPECT_FALSE(recipients.find(player3) == recipients.end());
+
+    response = controller.joinRoom(inviteCode, player4);
+    recipients = response.recipientList;
+
+    EXPECT_EQ(response.data.status, Status::SUCCESS);
+    EXPECT_EQ(recipients.size(), 4);
+    EXPECT_FALSE(recipients.find(player4) == recipients.end());
+
+    EXPECT_FALSE(recipients.find(player3) == recipients.end());
+    EXPECT_FALSE(recipients.find(player2) == recipients.end());
 }
