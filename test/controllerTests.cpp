c0ba16eaae466361344fb1f9e36b429251bf393d
hln5 <hln5@sfu.ca>
2022-11-16 18:45:04 -0800

    added unit test for creating room

--- test/controllerTests.cpp
+++ test/controllerTests.cpp
@@ -4,3 +4,35 @@
 #include "controller.h"
 
 using namespace testing;
+
+class ControllerTests : public Test {
+    protected:
+        void SetUp() override { }
+
+    Controller controller;
+    json config = json::parse(R"(
+        {
+            "configuration": {
+                "name": "Rock, Paper, Scissors",
+                "player count": {"min": 2, "max": 4},
+                "audience": false,
+                "setup": {
+                    "Rounds": 10
+                }
+            }
+        }
+    )");
+};
+
+TEST_F(ControllerTests, createRoom) {
+    networking::Connection player;
+    player.id = 111;
+
+    recipientsWrapper response = controller.createRoom(config, player);
+    auto recipients = response.recipientList;
+
+    EXPECT_EQ(response.data.status, Status::SUCCESS);
+    EXPECT_FALSE(response.data.message == ""); // game code should created
+    EXPECT_FALSE(recipients.find(player) == recipients.end());
+    EXPECT_EQ(recipients.size(), 1);
+}
